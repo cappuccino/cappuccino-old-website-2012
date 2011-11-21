ea0e7839ce8a7f27d551fd4840e1dc5ac09ce30e
Alexander Ljungberg <aljungberg@slevenbits.com>
2011-11-21 22:44:59 +0000

    Bring "Getting Setup" page into 2011.
    Replace highly outdates advice to use Firefox for debugging and fix other things.

--- public/learn/tutorials/getting-setup.php
+++ public/learn/tutorials/getting-setup.php
@@ -29,28 +29,17 @@ include('../../includes/header.php');
 
 <p>The browser is the most important part of the development cycle. Cappuccino apps target the browser just like Cocoa apps target Mac OS X. Choosing the right browser for your development cycle is a crucial step in the process.</p>
 
-<p>There's no one right answer to the question of which browser you should be using. There are four obvious contenders: Firefox, Safari, Chrome, and Internet Explorer.</p>
-
-<h3>Firefox</h3>
-
-<p>Many web developers today choose to go with the latest version of <a href="http://getfirefox.com">Firefox</a>. One of the biggest motivations for the choice is the availability of the Firebug extension. <a href="http://getfirebug.com">Firebug</a> gives you plenty of great features for debugging your applications like stack traces on exceptions, a JavaScript profiler for finding performance hot spots, and a monitor for all your http traffic, including XMLHTTPRequests. If you're going to use Firefox, Firebug is really an essential add-on to have.</p>
-
-<center><img src="images/error.png" alt="firebug errors" /></center>
-
-<p>Of all the browsers, Firefox also gives the most comprehensive error logs. In particular, parse errors tend to include specific issues and have helpful hints about where in the code to look for the problem. Other exceptions also usually report better errors than the other browsers. Even if you choose to use another browser for development, having a copy of Firefox around is good practice for solving hard to find problems.</p>
-
-<center><img src="images/net.png" alt="firebug http inespector" /></center>
+<p>There's no one right answer to the question of which browser you should be using. There are four obvious contenders: Safari, Chrome, Firefox and Internet Explorer.</p>
 
 <h3>Safari</h3>
 
-<p><a href="http://www.apple.com/safari/">Safari</a> is a somewhat less popular choice, especially because the shipping version of Safari has a somewhat poor set of developer tools. Fortunately that problem is being solved, and the <a href="http://nightly.webkit.org/">latest nightlies</a> have great additions to the Safari Web Inspector, including a debugger, profiler, and http monitor. To access the inspector, you'll need to check the "Show Develop menu in the menu bar" checkbox in the Advanced section of the preferences.</p>
+<p><a href="http://www.apple.com/safari/">Safari</a> is the most popular choice, especially because the shipping version of Safari has a great set of developer tools. There is a full featured debugger, profiler, and HTTP monitor. To access the inspector, you'll need to check the "Show Develop menu in the menu bar" checkbox in the Advanced section of the preferences.</p>
 
 <center><img src="images/develop1.png" alt="enable safari developer menu" /></center>
 
-<p>Safari does has a few advantages over Firefox. First, it's a faster browser, especially at JavaScript. Although both browsers are fast, when you're in the rapid development cycle of programming, refreshing, and testing your changes, the extra speed boost can be useful. Second, and perhaps more importantly, Safari has a slightly different security model than Firefox when it comes to XMLHTTPRequests and the same origin policy.</p>
+<p>Safari does have other advantages. First, it's a faster browser than Firefox, especially at JavaScript. Although both browsers are fast, when you're in the rapid development cycle of programming, refreshing, and testing your changes, the extra speed boost can be useful. Second, and perhaps more importantly, Safari has a slightly different security model than Firefox when it comes to XMLHTTPRequests and the same origin policy.</p>
 
-<p>In Safari, files running locally (i.e. file:///MyFolder/MyApplication/index.html) are allowed to access requests from any origin. This was designed so that Dashboard widgets could access external sites, but it's especially useful for application development.
-You can leave a working copy of your application on a separate webserver or development box, and use it as your backend for local development, without worrying about the same origin policy.</p>
+<p>In Safari, files running locally (i.e. file:///MyFolder/MyApplication/index.html) are allowed to access requests from any origin. This was designed so that Dashboard widgets could access external sites, but it's especially useful for application development. You can leave a working copy of your application on a separate webserver or development box, and use it as your backend for local development, without worrying about the same origin policy.</p>
 
 <p>It is also possible to configure Firefox 3 to behave similarly: <a href="http://wiki.github.com/280north/cappuccino/developing-locally-in-firefox-3">http://wiki.github.com/280north/cappuccino/developing-locally-in-firefox-3</a></p>
 
@@ -73,6 +62,16 @@ tusk install jack
 </code>
 </pre>
 
+<h3>Firefox</h3>
+
+<p>Some web developers choose to go with the latest version of <a href="http://getfirefox.com">Firefox</a>. Its Firebug extension was one of the first Javascript debugging toolkits. <a href="http://getfirebug.com">Firebug</a> gives you features for debugging your applications like stack traces on exceptions, a JavaScript profiler for finding performance hot spots, and a monitor for all your http traffic, including XMLHTTPRequests. If you're going to use Firefox, Firebug is really an essential add-on to have.</p>
+
+<center><img src="images/error.png" alt="firebug errors" /></center>
+
+<p>Sometimes Firefox gives the most comprehensive error logs. In particular, parse errors tend to include specific issues and have helpful hints about where in the code to look for the problem. Other exceptions also usually report better errors than the other browsers. Even if you choose to use another browser for development, having a copy of Firefox around is good practice for solving hard to find problems.</p>
+
+<center><img src="images/net.png" alt="firebug http inespector" /></center>
+
 <h3>Internet Explorer</h3>
 
 <p>Cappuccino works great in IE, and you should test your application in IE as often as possible. It is not recommended as a development environment though. IE's JavaScript interpreter is the slowest of the modern browsers, and it has some of the worst debugging tools.</p>
@@ -94,7 +93,7 @@ tusk install jack
     <li><a href="/files/objj.vim">VIM</a> - thanks to Shawn MacIntyre</li>
     <li><a href="/files/Cappuccino_Developer_Tools.pkg">Xcode</a> - thanks to Raphael Bartolome</li>
 </ul>
-<p>If you use another text editor, and have written or would like to write an add-on for it to support Objective-J, we strongly encourage it and will list it here. Get in touch with us at <a href="mailto:developers@280north.com">developers@280north.com</a>.</p>
+<p>If you use another text editor, and have written or would like to write an add-on for it to support Objective-J, we strongly encourage it and will list it here. Get in touch with us on the <a href="http://groups.google.com/group/objectivej">mailing list</a>.</p>
 
 <h2>Logging</h2>
 
@@ -122,7 +121,7 @@ http://localhost/MyApp/index-debug.html
 
 <h2>Additional Info</h2>
 
-<p>If you have suggestions for improvements to this guide, please send them to <a href="mailto:developers@280north.com">developers@280north.com</a>. If you have questions that haven't been answered here, you should try asking in the <a href="/discuss/list.php">mailing list or IRC channel</a>.</p>
+<p>If you have suggestions for improvements to this guide, or if you have questions that haven't been answered here, please let us know in the <a href="/discuss/list.php">mailing list or IRC channel</a>.</p>
 
 </div>
 
